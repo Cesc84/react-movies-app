9682744f88728a6e41cd24c1e2c7ed0e27d6018f
francesco daloe <MY_NAME@example.comfrancesco.d@tomorrowtth.com>
2020-04-14 19:23:59 +0100

    Updates

---
+++ precache-manifest.aa8373d0acaf4791a25d070780869a8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e648a739063d626b5f1ba5d4d2cc4a3",
+    "url": "/movies-app/index.html"
+  },
+  {
+    "revision": "bdffebdbb75e38395e5e",
+    "url": "/movies-app/static/css/main.05e900a2.chunk.css"
+  },
+  {
+    "revision": "b3b8f73085a2541d857f",
+    "url": "/movies-app/static/js/2.1f00e8be.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movies-app/static/js/2.1f00e8be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdffebdbb75e38395e5e",
+    "url": "/movies-app/static/js/main.e7da6dc8.chunk.js"
+  },
+  {
+    "revision": "7b61001d631c803aa746",
+    "url": "/movies-app/static/js/runtime-main.9adb8a0e.js"
+  }
+]);
